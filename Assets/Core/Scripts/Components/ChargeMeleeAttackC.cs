a052ac0b291252fec6ca5a244075d155918791cb
g_tretyak <137423551+g-tretyak@users.noreply.github.com>
2025-08-05 17:21:29 +0300

    небольшие фиксы 3

--- Assets/Core/Scripts/Components/ChargeMeleeAttackC.cs
+++ Assets/Core/Scripts/Components/ChargeMeleeAttackC.cs
@@ -1,4 +1,5 @@
 using System;
+using Core.Scripts.Plain;
 using FFS.Libraries.StaticEcs;
 
 namespace Core.Scripts
--- Assets/Core/Scripts/Components/ShortMeleeAttackC.cs
+++ Assets/Core/Scripts/Components/ShortMeleeAttackC.cs
@@ -1,78 +1,9 @@
 using System;
-using Core.Scripts.MasksTest;
+using Core.Scripts.Plain;
 using FFS.Libraries.StaticEcs;
-using UnityEngine;
-using ZeroZeroGames.Ecs.Modules.Shared.Components;
-using ZeroZeroGames.Ecs.Modules.Shared.Helpers;
-using ZeroZeroGames.Ecs.Modules.Shared.World;
 
 namespace Core.Scripts
 {
-    [Serializable]
-    public class MeleeModule : IAttack
-    {
-        public float val;
-
-        public TimerModule cooldownMod;
-        public TimerModule usingMod;
-
-        public MeleeModule(float val, float cooldownMaxVal, float usingVal)
-        {
-            this.val = val;
-            cooldownMod = new TimerModule(cooldownMaxVal);
-            usingMod = new TimerModule(usingVal);
-        }
-
-        public void OnTick(W.Entity ent)
-        {
-            cooldownMod.TickTimer();
-
-            var wasUsing = usingMod.OnTimer;
-
-            usingMod.TickTimer();
-
-            if (wasUsing && usingMod.OnTimer == false)
-            {
-                if (ent.HasAllOf<RootRigidbody2DC>() == false) return;
-                if (ent.HasAllOf<FlipC>() == false) return;
-                if (ent.HasAllOf<TeamC>() == false) return;
-
-                ref var rb = ref ent.Ref<RootRigidbody2DC>();
-                ref var flip = ref ent.Ref<FlipC>();
-                ref var team = ref ent.Ref<TeamC>();
-
-                var res = DoMeleeAttack.ProcessAttack(team.team, rb.val.position,
-                    flip.lastNonZeroX, DoMeleeAttack.GetEntDamage(ent));
-                if (res)
-                    EventHelpers.RequestLog("Hit success");
-                DoCleanup(ent);
-            }
-        }
-
-        public void DoCleanup(W.Entity ent)
-        {
-            ent.TryDeleteMask<Attacking>();
-        }
-
-        public void OnStart(W.Entity e)
-        {
-            ref var rb = ref e.Ref<RootRigidbody2DC>();
-            ref var flip = ref e.Ref<FlipC>();
-            rb.val.linearVelocityX = 0f;
-            rb.val.AddForceX(val * flip.lastNonZeroX, ForceMode2D.Impulse);
-            cooldownMod.SetTimer();
-            usingMod.SetTimer();
-
-            e.SetMask<Attacking>();
-        }
-
-        public bool CanUse()
-        {
-            return cooldownMod.OnTimer == false &&
-                   usingMod.OnTimer == false;
-        }
-    }
-
     [Serializable]
     public struct ShortMeleeAttackC : IComponent
     {
