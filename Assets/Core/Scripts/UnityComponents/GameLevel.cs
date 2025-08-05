b311fba8c45289172e46cf5448cf163731dd57a9
g_tretyak <137423551+g-tretyak@users.noreply.github.com>
2025-08-05 16:54:06 +0300

    папки рефакторинг

--- Assets/Core/Scripts/UnityComponents/GameLevel.cs
+++ Assets/Core/Scripts/UnityComponents/GameLevel.cs
@@ -5,7 +5,6 @@ namespace Core.Scripts
     public class GameLevel : MonoBehaviour
     {
         [SerializeField] private SpriteRenderer levelBounds;
-        //[SerializeField] private InteractableProvider[] interactables;
 
         public Bounds LevelBounds => levelBounds.bounds;
         public InteractableProvider[] Interactables => GetComponentsInChildren<InteractableProvider>();
